f5a527123d01dd7d0041c1bd2fb81b45912cf4ae
Carlos Puchol <cpg+git@amahi.org>
2013-01-11 00:57:20 -0800

    remove unused assets, change the layout to match the original theme and start a locale english file

--- app/controllers/greyhole_controller.rb
+++ app/controllers/greyhole_controller.rb
@@ -1,4 +1,3 @@
-#require_dependency "greyhole/application_controller"
 
 class GreyholeController < ApplicationController
 	before_filter :admin_required
@@ -11,9 +10,7 @@ class GreyholeController < ApplicationController
 		end
 		dppl = DiskPoolPartition.all
 		@broken_disk_pool_partitions = dppl.delete_if do |dpp|
-			! @partitions.select{|p| p[:path] == dpp.path}.empty?
+			! @partitions.select{ |p| p[:path] == dpp.path }.empty?
 		end
-		@partitions = [{:bytes_free=>21924315136, :device=>"/dev/sda", :path=>"/", :bytes_total=>111936864256}, {:bytes_free=>1313666871296, :device=>"/dev/sdd", :path=>"/home", :bytes_total=>1476758294528}, {:bytes_free=>67888459776, :device=>"/dev/sde", :path=>"/mnt/media", :bytes_total=>984505196544}, {:bytes_free=>59419136000, :device=>"/dev/sdc", :path=>"/mnt/1tb-sata-1", :bytes_total=>984506441728}, {:bytes_free=>394050961408, :device=>"/dev/sdb", :path=>"/mnt/1-5-tb-sata", :bytes_total=>1476756697088}]
 	end
-
 end

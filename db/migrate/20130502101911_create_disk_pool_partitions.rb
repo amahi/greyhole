bb5f783058fe317cd3ce8103b4162a00c63520f1
Carlos Puchol <cpg+git@amahi.org>
2014-02-16 00:02:55 -0800

    update locales, add drop table if exists

--- db/migrate/20130502101911_create_disk_pool_partitions.rb
+++ db/migrate/20130502101911_create_disk_pool_partitions.rb
@@ -1,5 +1,9 @@
 class CreateDiskPoolPartitions < ActiveRecord::Migration
 	def self.up
+
+		# protect against partial failures to uninstall
+		execute "DROP TABLE IF EXISTS 'disk_pool_partitions'"
+
 		create_table :disk_pool_partitions do |t|
 			t.string	:path
 			t.integer	:minimum_free, :default => 10
